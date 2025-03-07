b1e15785bf8660c9f47bf87e91f035376f7ca746
Phoenixnoir <phoenixnoirdev@users.noreply.github.com>
2025-03-07 01:14:02 +0100

    Update megaphone.cpp
    Fix saut de ligne en trop et espace en trop

--- CPP0/ex00/megaphone.cpp
+++ CPP0/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: phkevin <phkevin@42luxembourg.lu>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 10:22:01 by phkevin           #+#    #+#             */
-/*   Updated: 2025/01/24 11:26:23 by phkevin          ###   Luxembour.lu      */
+/*   Updated: 2025/03/07 11:26:23 by phkevin          ###   Luxembourg.lu     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <cctype>
 #include <cstring>
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     if (argc != 1)
     {
@@ -24,13 +24,10 @@ int main(int argc, char **argv)
             {
                 for (int ii = 0; ii < (int)strlen(argv[i]); ii++)
                     std::cout << (char)toupper(argv[i][ii]);
-                
-                if(i < (argc - 1))
-                    std::cout << " ";
             }
         }
     }
     else
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
     return 0;
 }
